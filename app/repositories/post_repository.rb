dfb31837360eba86db51952cfa2d2d462b9ee14c
Ivan Dolgov <idlhero@gmail.com>
2023-06-24 14:53:09 +0300

    Update post index sort

--- app/repositories/post_repository.rb
+++ app/repositories/post_repository.rb
@@ -6,5 +6,7 @@ module PostRepository
   included do
     scope :with_category, ->(category) { joins(:category).where(category: { id: category.id }) }
     scope :liked_by, ->(user) { joins(:likes).where(likes: { user_id: user.id }) }
+    scope :order_by_created_date_desc, -> { order(created_at: :desc) }
+    scope :order_by_created_date_asc, -> { order(created_at: :asc) }
   end
 end
